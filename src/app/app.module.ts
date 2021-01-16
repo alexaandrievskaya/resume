a29fec3483719db587750750df88d0900d6b01b6
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2021-01-16 20:03:36 +0200

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { WorkComponent } from './work/work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    WorkComponent,
+    SkillsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
